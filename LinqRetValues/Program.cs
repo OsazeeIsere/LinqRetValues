98ca39c80b409f853756ae876b0e038054c190b8
Osazee Isere <oz1.concepts@gmail.com>
2021-07-28 04:54:04 +0100

    intial commit

--- LinqRetValues/Program.cs
+++ LinqRetValues/Program.cs
@@ -10,6 +10,43 @@ namespace LinqRetValues
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("LINQ return values");
+            IEnumerable<string> subset = GetStringSubset();
+
+            foreach(string item in subset)
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.WriteLine("LINQ return values as array");
+
+            foreach (string item in GetStringSubsetAsArray()
+)
+            {
+                Console.WriteLine(item);
+
+            }
+
+            Console.ReadLine();
+
+        }
+
+        private static string[] GetStringSubsetAsArray()
+        {
+            string[] colors = { "Light Red", "Green", "Yellow", "Dark Red", "Red", "Purple" };
+
+            // Note the subset is an IEnumerable<string>- compactible object
+            var theRedColors = from c in colors where c.Contains("Red") select c;
+            return theRedColors.ToArray();
+        }
+
+        private static IEnumerable<string> GetStringSubset()
+        {
+            string[] colors = { "Light Red", "Green", "Yellow", "Dark Red", "Red", "Purple" };
+
+            // Note the subset is an IEnumerable<string>- compactible object
+            IEnumerable<string> theRedColors = from c in colors where c.Contains("Red") select c;
+            return theRedColors;
         }
     }
 }
